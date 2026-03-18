70987f9f1c7ccb5f67242609d190e5990492a3a0
kurosawanao <kurosawanao@team-lab.com>
2026-03-18 20:15:07 +0900

    suspense

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,27 +1,67 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { useSearchParams } from "next/navigation";
 import EmblaCarousel from "./slider/EmblaCarousel";
-import { EmblaOptionsType } from "embla-carousel";
-import Header from "./Header";
-import Footer from "./Footer";
-import "@/css/base.css";
-import "@/css/sandbox.css";
-import "@/css/embla.css";
+import { CSSProperties } from "react";
 
-const OPTIONS: EmblaOptionsType = { loop: true };
-const SLIDE_COUNT = 5;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+const DEFAULT_SLIDE_COUNT = 2;
 
-const App = () => (
-  <>
-    <Header />
-    <EmblaCarousel slides={SLIDES} options={OPTIONS} />
-    <Footer />
-  </>
-);
+const styles: CSSProperties[] = [
+  {
+    backgroundColor: "#64afff",
+    color: "#eee",
+  },
+  {
+    backgroundColor: "#ff4b40",
+    color: "#eee",
+  },
+  {
+    backgroundColor: "#b6ff40",
+    color: "#111",
+  },
+  {
+    backgroundColor: "#40f2f2",
+    color: "#111",
+  },
+  {
+    backgroundColor: "#ff409c",
+    color: "#eee",
+  },
+  {
+    backgroundColor: "#6474ff",
+    color: "#eee",
+  },
+];
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const params = useSearchParams();
+  const slideCount = Number(params.get("slideCount") ?? DEFAULT_SLIDE_COUNT);
+  const loop = params.get("loop") === "true";
+
+  return (
+    <>
+      <div>
+        <p># of slides: {slideCount}</p>
+        <p>Loop: {loop ? "Enabled" : "Disabled"}</p>
+      </div>
+      <EmblaCarousel
+        slides={Array.from({ length: slideCount }, (_, index) => (
+          <div
+            key={index}
+            style={{
+              ...styles[index % styles.length],
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {index + 1}
+          </div>
+        ))}
+        options={{ loop }}
+      />{" "}
+    </>
+  );
+};
+
+export default App;
--- app/page.tsx
+++ app/page.tsx
@@ -2,87 +2,21 @@
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import EmblaCarousel from "./components/slider/EmblaCarousel";
 
 import "@/css/base.css";
 import "@/css/sandbox.css";
 import "@/css/embla.css";
-import { useSearchParams } from "next/navigation";
-import { CSSProperties } from "react";
-
-const DEFAULT_SLIDE_COUNT = 2;
-
-const styles: CSSProperties[] = [
-  {
-    backgroundColor: "#64afff",
-    color: "#eee",
-  },
-  {
-    backgroundColor: "#ff4b40",
-    color: "#eee",
-  },
-  {
-    backgroundColor: "#b6ff40",
-    color: "#111",
-  },
-  {
-    backgroundColor: "#40f2f2",
-    color: "#111",
-  },
-  {
-    backgroundColor: "#ff409c",
-    color: "#eee",
-  },
-  {
-    backgroundColor: "#6474ff",
-    color: "#eee",
-  },
-];
+import App from "./components";
+import { Suspense } from "react";
 
 export default function Home() {
-  const params = useSearchParams();
-  const slideCount = Number(params.get("slideCount") ?? DEFAULT_SLIDE_COUNT);
-  const loop = params.get("loop") === "true";
-
   return (
     <>
       <Header />
-      <div>
-        <p># of slides: {slideCount}</p>
-        <p>Loop: {loop ? "Enabled" : "Disabled"}</p>
-      </div>
-      <EmblaCarousel
-        slides={Array.from({ length: slideCount }, (_, index) => (
-          <div
-            key={index}
-            style={{
-              ...styles[index % styles.length],
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {index + 1}
-          </div>
-        ))}
-        options={{ loop }}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
       <Footer />
     </>
   );
-}
-
-function calcLuminance(color: string) {
-  const [r, g, b] = [
-    color.slice(1, 3),
-    color.slice(3, 5),
-    color.slice(5, 7),
-  ].map((hex) => parseInt(hex, 16) / 255);
-  return 0.2126 * r + 0.7152 * g + 0.0722 * b;
-}
-
-function calcTextColor(color: string) {
-  return calcLuminance(color) < 0.6 ? "#eee" : "#111";
 }
